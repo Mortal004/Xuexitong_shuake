be0972856c806af04eccc5f8d6fab441deec3456
Mortal <2022865286@qq.com>
2025-02-12 22:57:02 +0800

    Update getanswer.py

--- task/tool/getanswer.py
+++ task/tool/getanswer.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 Mortal004
+# All rights reserved.
+# This software is provided for non-commercial use only.
+# For more information, see the LICENSE file in the root directory of this project.
+
 import io
 import re
 import sys
